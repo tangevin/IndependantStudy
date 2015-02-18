126bcdaed58839b72cbaaf6ac88984171d8117b3
Ethan Nakashima <ethanmn@hotmail.com>
2015-02-17 17:05:16 -0800

    Added menus

---
+++ Assets/Scripts/PilotMenuBox.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PilotMenuBox : MonoBehaviour {
+
+	public Sprite idle;
+	public Sprite hover;
+	public Sprite select;
+	
+	private SpriteRenderer sr;
+	private bool down;
+
+	// Use this for initialization
+	void Start () {
+		sr = gameObject.GetComponent<SpriteRenderer>();
+		down = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseOver() 
+	{
+		if (!down)
+			sr.sprite = hover;
+	}
+	
+	void OnMouseExit()
+	{
+		sr.sprite = idle;
+	}
+	
+	void OnMouseDown()
+	{
+		down = true;
+		sr.sprite = select;
+	}
+	
+	void OnMouseUp()
+	{
+		down = false;
+	}
+}
