d72d9d5aed0572e78b313b1ff70c8b5b047a7ab3
xeon13 <xeonbaseball13@gmail.com>
2015-03-08 12:00:35 -0700

    Added weapon

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Weapon : MonoBehaviour {
+	private int damage;
+	private int range;
+	private int fireRate;
+	private int ammoCapacity;
+	private int reloadSpeed;
+
+	public int getDamage() {
+		return this.damage;
+	}
+
+	public int getRange() {
+		return this.range;
+	}
+
+	public int getFireRate() {
+		return this.fireRate;
+	}
+
+	public int getAmmoCapacity() {
+		return this.ammoCapacity;
+	}
+
+	public int getReloadSpeed() {
+		return this.reloadSpeed;
+	}
+}
